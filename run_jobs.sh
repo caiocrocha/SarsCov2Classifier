c1a3de525cc7be2798b9a9655989596d66e0ba62
caiocedrola <caiocedrola@ice.ufjf.br>
2020-11-26 02:58:59 -0200

    Scores data do cluster

--- run_jobs.sh
+++ run_jobs.sh
@@ -8,7 +8,5 @@ DIR="MODELS"
 for entry in ${DIR}/* ; do
     if [[ ! -f ${entry}/score.csv && -f ${entry}/job.sh ]] ; then
         qsub -q all.q@compute-1* ${entry}/job.sh 2>&1 || exit 1
-    else
-        exit 1
     fi
 done
